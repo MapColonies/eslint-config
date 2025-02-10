1af71b57a425adcd927fd0cb6d4404608403157d
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-02-10 15:37:48 +0200

    chore: more work

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,17 +1,43 @@
-/** @type {import('eslint').ESLint.ConfigData} */
-export default {
-  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'prettier'],
-  env: {
-    browser: true,
-  },
-  rules: {
-    'react/boolean-prop-naming': 'error',
-    'react/hook-use-state': 'error',
-    'react/prop-types': 'off',
+import { browser } from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import { config } from '../helpers';
+// export default {
+//   extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
+//   env: {
+//     browser: true,
+//   },
+//   rules: {
+//     'react/boolean-prop-naming': 'error',
+//     'react/hook-use-state': 'error',
+//     'react/prop-types': 'off',
+//   },
+//   settings: {
+//     react: {
+//       version: 'detect',
+//     },
+//   },
+// };
+
+// https://github.com/facebook/react/pull/28773#issuecomment-2422831582
+
+const reactRules = config({
+  name: 'map-colonies react rules',
+  files: ['**/*.tsx'],
+  extends: [reactPlugin.configs.flat.recommended ?? {}, reactPlugin.configs.flat.jsxRuntime ?? {}],
+  languageOptions: {
+    globals: browser,
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-};
+  rules: {
+    'react/boolean-prop-naming': 'error',
+    'react/hook-use-state': 'error',
+    'react/prop-types': 'off',
+  },
+});
+
+export default config(reactRules);
--- src/configs/ts-base.ts
+++ src/configs/ts-base.ts
@@ -2,7 +2,9 @@ import type { Linter } from 'eslint';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginImportX from 'eslint-plugin-import-x';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import tsParser from '@typescript-eslint/parser';
+import { config } from '../helpers';
 
 export const namingConventions = [
   'error',
@@ -43,157 +45,131 @@ export const namingConventions = [
   },
 ] as const satisfies Linter.RuleEntry;
 
-const lol = tseslint.config(
+const typescriptEslintConfig = config({
+  name: 'map-colonies typescript rules',
+  rules: {
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-tslint-comment': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/member-ordering': 'warn',
+    '@typescript-eslint/method-signature-style': 'error',
+    '@typescript-eslint/no-empty-interface': 'off',
+    curly: 'error',
+    camelcase: 'off',
+    'no-lonely-if': 'error',
+    '@typescript-eslint/naming-convention': namingConventions,
+    '@typescript-eslint/no-base-to-string': 'warn',
+    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+    '@typescript-eslint/no-extraneous-class': 'warn',
+    // '@typescript-eslint/no-throw-literal': 'error',
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
+    '@typescript-eslint/prefer-literal-enum-member': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    '@typescript-eslint/require-array-sort-compare': 'warn',
+    '@typescript-eslint/strict-boolean-expressions': 'warn',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/no-magic-numbers': [
+      'error',
+      {
+        ignoreArrayIndexes: true,
+        ignore: [1, 0],
+        ignoreNumericLiteralTypes: true,
+      },
+    ],
+    '@typescript-eslint/default-param-last': 'error',
+    '@typescript-eslint/no-dupe-class-members': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+    '@typescript-eslint/return-await': 'error',
+  },
+});
+
+// This is not in the jest config as it only turns rules off, and needs to be applied after the other rules
+const jestTurnedOffRules = config({
+  name: 'map-colonies disabled rules for jest',
+  files: ['**/*.spec.ts?(x)', '**/*.test.ts?(x)'],
+  rules: {
+    '@typescript-eslint/no-magic-numbers': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+});
+
+// // This is not in the react config as it only turns rules off, and needs to be applied after the other rules
+const reactNamingConventions = config({
+  name: 'map-colonies react naming conventions',
+  files: ['**/*.tsx'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      ...namingConventions,
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase'],
+        types: ['function'],
+      },
+    ],
+  },
+});
+
+const importRulesAndConfig = config({
+  name: 'map-colonies import-x rules',
+  files: ['**/*.ts?(x)'],
+  ignores: ['eslint.config.*'],
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import-x/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '\@**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '\@*/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+    'import-x/first': 'error',
+    'import-x/exports-last': 'error',
+    'import-x/newline-after-import': 'error',
+  },
+});
+
+const globalIgnoreConfig = config({
+  name: 'map-colonies global ignore',
+  ignores: ['.husky', 'coverage', 'reports', 'dist', 'node_modules', '**/*.{js,mjs,cjs}', 'helm'],
+});
+
+const combinedConfig = config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
-  {
-    rules: {
-      '@typescript-eslint/array-type': ['error', { default: 'array' }],
-      '@typescript-eslint/ban-ts-comment': 'warn',
-      '@typescript-eslint/ban-tslint-comment': 'error',
-      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/explicit-member-accessibility': 'error',
-      '@typescript-eslint/member-ordering': 'warn',
-      '@typescript-eslint/method-signature-style': 'error',
-      '@typescript-eslint/no-empty-interface': 'off',
-      // 'brace-style': 'off',
-      // '@typescript-eslint/brace-style': ['error'], MOVED TO STYLISTIC
-      curly: 'error',
-      camelcase: 'off',
-      'no-lonely-if': 'error',
-      '@typescript-eslint/naming-convention': namingConventions,
-      '@typescript-eslint/no-base-to-string': 'warn',
-      '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-      '@typescript-eslint/no-extraneous-class': 'warn',
-      // '@typescript-eslint/no-throw-literal': 'error',
-      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-      '@typescript-eslint/no-unnecessary-condition': 'error',
-      '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
-      '@typescript-eslint/prefer-literal-enum-member': 'error',
-      '@typescript-eslint/prefer-nullish-coalescing': 'warn',
-      '@typescript-eslint/prefer-optional-chain': 'warn',
-      '@typescript-eslint/prefer-readonly': 'error',
-      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-      '@typescript-eslint/promise-function-async': 'error',
-      '@typescript-eslint/require-array-sort-compare': 'warn',
-      '@typescript-eslint/strict-boolean-expressions': 'warn',
-      '@typescript-eslint/switch-exhaustiveness-check': 'error',
-      '@typescript-eslint/no-magic-numbers': [
-        'error',
-        {
-          ignoreArrayIndexes: true,
-          ignore: [1, 0],
-          ignoreNumericLiteralTypes: true,
-        },
-      ],
-      '@typescript-eslint/default-param-last': 'error',
-      '@typescript-eslint/no-dupe-class-members': 'error',
-      '@typescript-eslint/no-unused-expressions': 'error',
-      '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
-      '@typescript-eslint/return-await': 'error',
-    },
-  },
-
-  {
-    files: ['**/*.spec.ts?(x)', '**/*.test.ts?(x)'],
-    rules: {
-      '@typescript-eslint/no-magic-numbers': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    extends: [tseslint.configs.disableTypeChecked],
-  },
+  typescriptEslintConfig,
+  jestTurnedOffRules,
+  reactNamingConventions,
   eslintPluginImportX.flatConfigs.recommended,
   eslintPluginImportX.flatConfigs.typescript,
-  {
-    files: ['**/*.{ts,tsx}'],
-    ignores: ['eslint.config.*'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'import-x/order': [
-        'error',
-        {
-          pathGroups: [
-            {
-              pattern: '\@*/**',
-              group: 'external',
-              position: 'after',
-            },
-            {
-              pattern: '\@**',
-              group: 'external',
-              position: 'after',
-            },
-          ],
-        },
-      ],
-      'import-x/first': 'error',
-      'import-x/exports-last': 'error',
-      'import-x/newline-after-import': 'error',
-    },
-  },
-  { ignores: ['.husky', 'coverage', 'reports', 'dist', 'node_modules'] }
+  importRulesAndConfig,
+  globalIgnoreConfig,
+  eslintConfigPrettier
 );
 
-// export default [
-//   {
-//     // extends: [
-//     //   'eslint:recommended',
-//     //   'plugin:@typescript-eslint/recommended',
-//     //   'plugin:@typescript-eslint/recommended-requiring-type-checking',
-//     //   'prettier',
-//     // ],
-//     // plugins: ['import', '@typescript-eslint'],
-//     languageOptions: {
-//       parser: '@typescript-eslint/parser',
-//       parserOptions: {
-//         ecmaVersion: 2018,
-//         sourceType: 'module',
-//       },
-//     },
-//     env: {
-//       browser: true,
-//       es6: true,
-//       node: true,
-//     },
-
-//     // overrides for TSX (mostly for react)
-//     overrides: [
-//       {
-//         files: ['**/*.tsx'],
-//         rules: {
-//           '@typescript-eslint/naming-convention': [
-//             ...namingConventions,
-//             {
-//               selector: 'variable',
-//               types: ['function'],
-//               format: ['camelCase', 'PascalCase'],
-//             },
-//           ],
-//         },
-//       },
-//       // overrides for storybook
-//       {
-//         files: ['**/*.stories.ts?(x)'],
-//         rules: {
-//           '@typescript-eslint/no-magic-numbers': 'off',
-//         },
-//       },
-//       // overrides for unit tests
-//       {
-//         files: ['**/*.spec.ts?(x)', '**/*.test.ts?(x)'],
-//         rules: {
-//           '@typescript-eslint/no-magic-numbers': 'off',
-//           '@typescript-eslint/explicit-function-return-type': 'off',
-//         },
-//       },
-//     ],
-export default lol;
-module.exports = lol;
+export default combinedConfig;
+module.exports = combinedConfig;
